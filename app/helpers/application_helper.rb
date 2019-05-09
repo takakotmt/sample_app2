347b8fd1e19b2b7efff058823c9a75731c254a72
takakotmt <st.0614.tmt@gmail.com>
2019-05-09 12:55:21 +0900

    Make a basic User model (including secure passwords)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
-  # ページごとの完全なタイトルを返します。
- def full_title(page_title = '')
-   base_title = "Ruby on Rails Tutorial Sample App"
-   if page_title.empty?
-     base_title
-   else
-     page_title + " | " + base_title
-   end
- end
-=======
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
@@ -18,5 +7,4 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
->>>>>>> filling-in-layout
 end
