7447bd0549dec562efe5cce93f4be2d56774213c
takakotmt <st.0614.tmt@gmail.com>
2019-05-09 16:26:03 +0900

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -21,8 +21,9 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
@@ -30,8 +31,11 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-# preload_app!
+preload_app!
 
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
 # are forked to prevent connection leakage.
@@ -47,9 +51,13 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # or connections that may have been created at application boot, as Ruby
 # cannot share connections between processes.
 #
-# on_worker_boot do
+on_worker_boot do
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
+# Worker specific setup for Rails 4.1+
+# See: https://devcenter.heroku.com/articles/
+# deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
 #
 
 # Allow puma to be restarted by `rails restart` command.
