21f50b8ec4aec1dd97e3d80c1d782be2553b3efc
takakotmt <st.0614.tmt@gmail.com>
2019-05-13 14:59:47 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'bootstrap-sass', '3.3.7'
 gem 'jquery-rails', '4.3.1'
+gem 'faker',          '1.7.3'
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -29,7 +29,7 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-  
+
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
